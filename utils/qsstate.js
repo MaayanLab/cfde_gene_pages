80d5c21ebd7e4d5065c6747837c525dd72f1bcb6
Daniel Clarke <u8sand@gmail.com>
2021-08-18 09:59:50 -0400

    qsstate fixes

--- utils/qsstate.js
+++ utils/qsstate.js
@@ -7,14 +7,17 @@ export function useQsState(paramName, initialState, Codec) {
   const router = useRouter()
   const [param, setParam] = React.useState(paramName in router.query ? Codec.parse(router.query[paramName]) : initialState)
   React.useEffect(() => {
-    if (paramName in router.query && router.query[paramName] !== Codec.stringify(param)) {
-      setParam(Codec.parse(router.query[paramName]))
+    const paramString = paramName in router.query ? router.query[paramName] : initialStateString
+    if (paramString !== Codec.stringify(param)) {
+      setParam(Codec.parse(paramString))
     }
   }, [router.query])
   React.useEffect(() => {
     let paramString = Codec.stringify(param)
     if (paramString === initialStateString) {
-      paramString = undefined
+      if (router.pathname.indexOf(`[${paramName}]`) === -1) {
+        paramString = undefined
+      }
     }
     if (router.query[paramName] !== paramString) {
       const query = {...router.query}
