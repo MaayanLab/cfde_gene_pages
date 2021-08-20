9a93cc1d247ab2f7e9efb70d31925ce854a14da9
Daniel Clarke <u8sand@gmail.com>
2021-08-20 15:53:03 -0400

    Use getStaticProps for gene/drug resource pages

--- pages/[entity]/index.jsx
+++ pages/[entity]/index.jsx
@@ -1,23 +1,51 @@
-import manifest from '@/manifest'
+import full_manifest from '@/manifest'
 import dynamic from 'next/dynamic'
 
 const EntityCard = dynamic(() => import('@/components/EntityCard'))
 
-let entities = {'gene': true, 'drug': true}
-
-export function getServerSideProps({ query: { entity } }) {
-  if (!(entity in entities)) return { notFound: true }
-  return { props: { entity } }
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { entity: 'gene' } },
+      { params: { entity: 'drug' } },
+    ],
+    fallback: false,
+  }
 }
 
-export default function Entity({ entity }) {
+export async function getStaticProps({ params: { entity } }) {
+  if (entity !== 'gene' && entity !== 'drug') return { notFound: true }
+  const manifest = (
+    await Promise.all(
+      full_manifest
+        .map(async (item) => {
+          if (!(entity in item.tags)) return
+          try {
+            const resolved_item = { ...item }
+            delete resolved_item.countapi
+            delete resolved_item.clickurl
+            resolved_item.clicks = await item.countapi.get()
+            return resolved_item
+          } catch (e) {
+            console.warn(`${item.name} was not resolved: ${e}`)
+            return
+          }
+        })
+    )
+  ).filter(v => v !== undefined)
+  return { props: { entity, manifest }, revalidate: 60 }
+}
+
+export default function Entity(props) {
   return (
     <div className="album py-5 bg-light">
       <div className="container">
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 justify-content-center">
-          {manifest.filter(props => entity in props.tags).map((props) => (
-            <EntityCard key={props.name} {...props} />
-          ))}
+          {manifest
+            .filter(item => props.entity in item.tags)
+            .map(item => (
+              <EntityCard key={item.name} {...item} />
+            ))}
         </div>
       </div>
     </div>
