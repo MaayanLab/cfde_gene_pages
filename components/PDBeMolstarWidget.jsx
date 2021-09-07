f4ab51143e3c0b8b537a1e7ea5a6f1eabf1ffb8b
Daniel Clarke <u8sand@gmail.com>
2021-09-07 12:54:22 -0400

    Added PDBeMolestar component

---
+++ components/PDBeMolstarWidget.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Head from 'next/head'
+
+const PDBeMolstarPluginContext_ = React.createContext()
+
+export function PDBeMolstarPluginContext({ children }) {
+  const [instance, setInstance] = React.useState()
+  React.useEffect(() => {
+    setInstance(new PDBeMolstarPlugin())
+  }, [])
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" type="text/css" href="https://www.ebi.ac.uk/pdbe/pdb-component-library/css/pdbe-molstar-1.1.0.css" />
+        <script type="text/javascript" src="https://www.ebi.ac.uk/pdbe/pdb-component-library/js/pdbe-molstar-plugin-1.1.0.js"></script>
+      </Head>
+      <PDBeMolstarPluginContext_.Provider value={instance}>
+        {children}
+      </PDBeMolstarPluginContext_.Provider>
+    </>
+  )
+}
+
+export default function PDBeMolstarWidget({ options, style, ...props }) {
+  const ref = React.useRef()
+  const instance = React.useContext(PDBeMolstarPluginContext_)
+  React.useEffect(() => {
+    if (!ref || !ref.current || !instance || !options) return
+    instance.render(ref.current, options)
+  }, [ref, instance, options])
+  if (style === undefined) style = {}
+  return (
+    <div ref={ref} style={{...style, position: 'relative', flex: '1 0 auto' }} {...props} />
+  )
+}
