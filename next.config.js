75fb54521ee3881b9921f029dbd5f23586b99bf9
Daniel Clarke <u8sand@gmail.com>
2021-10-14 17:10:37 -0400

    Add a way to build additional scripts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
+const path = require('path')
+const fs = require('fs')
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 module.exports = {
   publicRuntimeConfig: {
     origin: 'https://cfde-gene-pages.maayanlab.cloud',
     gaId: 'G-J69H7NSWHW',
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      return {
+        ...config,
+        entry() {
+          return config.entry().then(entry => {
+            const _entry = {...entry}
+            for (const file of fs.readdirSync(path.resolve('services'))) {
+              _entry[path.basename(file, '.js')] = path.resolve('services', file)
+            }
+            return _entry
+          })
+        }
+      }
+    } else {
+      return config
+    }
   }
 }
