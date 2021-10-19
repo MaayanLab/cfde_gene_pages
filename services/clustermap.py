8e30bf8f0f999d8d0741b2957780ca3d6d84bf78
Daniel J. B. Clarke <u8sand@gmail.com>
2021-10-19 16:16:39 -0400

    Add python script to produce clustermap

---
+++ services/clustermap.py
@@ -0,0 +1,23 @@
+import click
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+@click.command()
+@click.option('-i', '--input', type=click.Path(file_okay=True), help='Dataframe')
+@click.option('-o', '--output', type=click.Path(), help='Figure')
+def clustermap(input, output):
+  df = pd.read_csv(input, sep='\t', index_col=0)
+  g = sns.clustermap(
+    df.T,
+    col_cluster=False,
+    row_colors=None,
+    yticklabels=True,
+    xticklabels=False,
+  )
+  g.ax_cbar.set_visible(False)
+  g.ax_col_dendrogram.set_visible(False)
+  plt.savefig(output, dpi=300, bbox_inches='tight')
+
+if __name__ == '__main__':
+  clustermap()
