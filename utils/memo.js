7fa7a64c61631f5cbf032ef1e61c4aa5f03459ea
Daniel Clarke <u8sand@gmail.com>
2022-01-06 14:58:27 -0500

    Propagate errors in `memo`

--- utils/memo.js
+++ utils/memo.js
@@ -1,12 +1,9 @@
 export default function memo(func) {
   const func_cache = {}
-  function memoFunc(...args) {
+  async function memoFunc(...args) {
     const serialized_args = JSON.stringify(args)
     if (!(serialized_args in func_cache)) {
-      func_cache[serialized_args] = func(...args).then(ret => {
-        func_cache[serialized_args] = ret
-        return ret
-      })
+      func_cache[serialized_args] = await func(...args)
     }
     return func_cache[serialized_args]
   }
