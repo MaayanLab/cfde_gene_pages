14292bfa249d89ea6eac2c348ab6bbe3029a1c1a
Daniel Clarke <u8sand@gmail.com>
2022-01-31 10:31:18 -0500

    Add tooltip with descriptions
    Fixes #158

---
+++ components/BootstrapTooltip.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { Tooltip } from "bootstrap"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
+
+export default function BootstrapTooltip({ title }) {
+  const ref = React.useRef()
+  React.useEffect(() => {
+    if (ref.current === null) return
+    new Tooltip(ref.current)
+  }, [ref])
+  return (
+    <span
+      ref={ref}
+      className="d-inline-block"
+      tabindex="0"
+      data-bs-toggle="popover"
+      data-bs-trigger="hover focus"
+      title={title}>
+      <FontAwesomeIcon width={16} height={16} icon={faQuestionCircle} />
+    </span>
+  )
+}
--- components/SimilarityInfo.jsx
+++ components/SimilarityInfo.jsx
@@ -1,9 +1,17 @@
+import dynamic from 'next/dynamic'
+
+const BootstrapTooltip = dynamic(() => import('@/components/BootstrapTooltip'), { ssr: false })
 
 export default function SimilarityInfo({ router, title, entity, items, description }) {
   return (
     <div className="col-12 justify-content-center">
       <p>
-        <span style={{ fontWeight: 500 }}>{title}: </span>
+        <span style={{ fontWeight: 500, whiteSpace: 'nowrap' }}>
+          {title}
+          &nbsp;
+          <BootstrapTooltip title={description} />
+          :&nbsp;
+        </span>
         {items.map(item => (
           <a
             key={item}
