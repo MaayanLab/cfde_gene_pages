cdb6d344908c5af03d8cdb0c94e8353343afb301
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2022-02-01 15:12:51 -0500

    Make question mark icons subtle

--- components/BootstrapTooltip.jsx
+++ components/BootstrapTooltip.jsx
@@ -1,27 +1,29 @@
 import React from "react"
-import { Tooltip } from "bootstrap"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
+import {Tooltip} from "bootstrap"
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faQuestionCircle} from '@fortawesome/free-solid-svg-icons'
 
-export default function BootstrapTooltip({ title }) {
-  const ref = React.useRef()
-  React.useEffect(() => {
-    if (ref.current === null) return
-    new Tooltip(ref.current)
-  }, [ref])
-  return (
-    <span
-      ref={ref}
-      className="d-inline-block"
-      tabindex="0"
-      data-bs-toggle="popover"
-      data-bs-trigger="hover focus"
-      title={title}>
-      <FontAwesomeIcon
-        width={24} height={24}
-        icon={faQuestionCircle}
-        transform="up-1 left-1 shrink-4"
-      />
+export default function BootstrapTooltip({title}) {
+    const ref = React.useRef()
+    React.useEffect(() => {
+        if (ref.current === null) return
+        new Tooltip(ref.current)
+    }, [ref])
+    return (
+        <span
+            ref={ref}
+            className="d-inline-block"
+            tabindex="0"
+            data-bs-toggle="popover"
+            data-bs-trigger="hover focus"
+            title={title}>
+            <sup>
+                <FontAwesomeIcon
+                    width={16} height={16}
+                    icon={faQuestionCircle}
+                    transform="up-1 left-1 shrink-4"
+                />
+            </sup>
     </span>
-  )
+    )
 }
