47cb35f267dfc019a545c22b36d21a6f387f7b7b
Daniel Clarke <u8sand@gmail.com>
2022-05-05 10:40:22 -0400

    Fix next build

--- next.config.js
+++ next.config.js
@@ -1,30 +1,7 @@
-const path = require('path')
-const fs = require('fs')
-
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 module.exports = {
   publicRuntimeConfig: {
     origin: 'https://cfde-gene-pages.maayanlab.cloud',
     gaId: 'G-J69H7NSWHW',
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      return {
-        ...config,
-        entry() {
-          return config.entry().then(entry => {
-            const _entry = {...entry}
-            for (const file of fs.readdirSync(path.resolve('services'))) {
-              if (file.endsWith('.js')) {
-                _entry[path.basename(file, '.js')] = path.resolve('services', file)
-              }
-            }
-            return _entry
-          })
-        }
-      }
-    } else {
-      return config
-    }
-  }
 }
