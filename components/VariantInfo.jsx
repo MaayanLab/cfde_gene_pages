70fa24ee2a3df8263418bae3e3bd1f99077cc00e
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2022-06-06 15:07:41 -0400

    Update for deployment

--- components/VariantInfo.jsx
+++ components/VariantInfo.jsx
@@ -6,7 +6,7 @@ export default function VariantInfo({ variant, gene, description }) {
   return (
     <div className="col-12 justify-content-center">
       <p>
-          The variant <span style={{ fontWeight: 600, whiteSpace: 'nowrap' }}>{variant}</span> was converted to a gene <a href={`https://cfde-gene-pages-variants.maayanlab.cloud/gene/${gene}?CF=false&PS=true`} target='_blank'>{gene}</a> using dbSNP&nbsp;<BootstrapTooltip title={description} />
+          The variant <span style={{ fontWeight: 600, whiteSpace: 'nowrap' }}>{variant}</span> was converted to a gene <a href={`https://cfde-gene-pages.maayanlab.cloud/gene/${gene}?CF=false&PS=true`} target='_blank'>{gene}</a> using dbSNP&nbsp;<BootstrapTooltip title={description} />
       </p>
     </div>
   )
